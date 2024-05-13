afb9505a77013bb2c1f2e01120a772df909daaab
Donglin Zhang <Donglin.Zhang@newaim.com.au>
2024-05-13 17:31:43 +1000

    add check on npx command args

--- bin/npxRun.js
+++ bin/npxRun.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
-
-
 const {secretManager} = require('../src/secret');
 
-const key = process.argv[2];
-const filepath = process.argv[3];
+const args = process.argv;
+// must have key specified
+const keyArg = args.find(arg => arg.includes('--key'));
+if (!keyArg) throw new Error('Key must be specified by --key');
+const key = keyArg.split('=')[1];
+if (!key || !key.trim()) throw new Error('Key cannot be empty');
+
+// must have filepath specified
+const pathArg = args.find(arg => arg.includes('--path'));
+if (!pathArg) throw new Error('Path must be specified by --path');
+const filepath = pathArg.split('=')[1];
+if (!filepath || !filepath.trim()) throw new Error('Path cannot be empty');
 
 console.log(`Encrypting file ${filepath} with key provided...`);
 secretManager.saveSecret(filepath, key);
 console.log(`file been secreted and saved to ${filepath}_sec`);
+secretManager.readSecret(filepath+'_sec', key);
