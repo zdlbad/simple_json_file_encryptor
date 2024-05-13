1894acb23b92871fdc6a64a433cd1627d3441f67
Donglin Zhang <Donglin.Zhang@newaim.com.au>
2024-05-13 16:37:19 +1000

    add npm package; add npx command

---
+++ npxRun.js
@@ -0,0 +1,8 @@
+const {secretManager} = require('./src/secret');
+
+const key = process.argv[2];
+const filepath = process.argv[3];
+
+console.log(`Encrypting file ${filepath} with key provided...`);
+secretManager.saveSecret(filepath, key);
+console.log(`file been secreted and saved to ${filepath}_sec`);
