4624cc03562f9d301cde9d417192a1659080e696
Donglin Zhang <Donglin.Zhang@newaim.com.au>
2024-05-13 17:40:57 +1000

    update args style from npx command

--- bin/npxRun.js
+++ bin/npxRun.js
@@ -3,16 +3,16 @@ const {secretManager} = require('../src/secret');
 
 const args = process.argv;
 // must have key specified
-const keyArg = args.find(arg => arg.includes('--key'));
-if (!keyArg) throw new Error('Key must be specified by --key');
-const key = keyArg.split('=')[1];
-if (!key || !key.trim()) throw new Error('Key cannot be empty');
+const keyArgIndex = args.indexOf('--key');
+if (keyArgIndex < 0) throw new Error('Key must be specified by and after --key');
+const key = args[keyArgIndex+1];
+if (!key || !key.trim() || key.includes('--')) throw new Error('Key cannot be empty');
 
 // must have filepath specified
-const pathArg = args.find(arg => arg.includes('--path'));
-if (!pathArg) throw new Error('Path must be specified by --path');
-const filepath = pathArg.split('=')[1];
-if (!filepath || !filepath.trim()) throw new Error('Path cannot be empty');
+const pathArgIndex = args.indexOf('--path');
+if (!pathArgIndex) throw new Error('Path must be specified by and after --path');
+const filepath = args[pathArgIndex+1];
+if (!filepath || !filepath.trim() || filepath.includes('--')) throw new Error('Path cannot be empty');
 
 console.log(`Encrypting file ${filepath} with key provided...`);
 secretManager.saveSecret(filepath, key);
